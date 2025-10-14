ee6499f858579442369ba20d5ec0aac846f0c346
zoxknez <zoxknez@hotmail.com>
2025-10-14 19:45:01 +0200

    feat: Add lib/index.ts exports and install tesseract.js for OCR

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+// Centralized exports from lib/
+export * from './analytics'
+export * from './categories'
+export * from './notifications'
+export * from './ocr'
+export * from './qr-scanner'
+export * from './utils'
+export * from './validation'
+export * from './db'
